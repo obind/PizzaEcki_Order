fe396147c9c47b85304e984ec8d8db6a23fa7a39
admin <admin@DESKTOP-1POTK89>
2023-10-28 13:54:46 -0700

    alle gerichte hinzugefügt mit passenden kategorien

--- PizzaEcki/Models/DishSizeManager.cs
+++ PizzaEcki/Models/DishSizeManager.cs
@@ -13,7 +13,15 @@ namespace PizzaEcki.Models
             { DishCategory.Pizza, new List<string> { "S", "L", "XL" } },
             { DishCategory.Salate, new List<string> { "S", "L" } },
             { DishCategory.Nudeln, new List<string> { "L" } },
-            {DishCategory.Döner, new List<string> { "L",} }
+            {DishCategory.Döner, new List<string> { "L",} },
+            {DishCategory.Baguette, new List<string> {"S", "L",} },
+            {DishCategory.Pasta,new List<string> { "L",} },
+            {DishCategory.Omelette,new List<string> { "L",} },
+            {DishCategory.Getränke,new List<string> { "L",} },
+            {DishCategory.Schnitzel,new List<string> { "L",} },
+            {DishCategory.TexMex,new List<string> { "L",} },
+            {DishCategory.Extras,new List<string> { "L",} },
+            {DishCategory.Kartoffelaufläufe,new List<string> { "L",} }
             // Weitere Kategorien und Größen können hier hinzugefügt werden
         };
     }
