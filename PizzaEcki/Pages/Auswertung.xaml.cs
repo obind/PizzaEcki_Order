b209f49dbf2db464368460a7f6bf568ef1be65f7
Kenneth Ballen <kennethbakken251@gmail.com>
2024-02-13 22:55:38 +0100

    Fahrerstatisitk ist nun druckbar

--- PizzaEcki/Pages/Auswertung.xaml.cs
+++ PizzaEcki/Pages/Auswertung.xaml.cs
@@ -11,6 +11,8 @@ using System.Drawing.Printing; // Für den Druck
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
+
+
 namespace PizzaEcki.Pages
 {
     /// <summary>
@@ -47,5 +49,49 @@ namespace PizzaEcki.Pages
             }
             return totalSales;
         }
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDocument printDoc = new PrintDocument();
+            printDoc.DefaultPageSettings.PaperSize = new PaperSize("Receipt", 300, 10000); // A4-Größe in 1/100 Zoll
+            printDoc.PrintPage += (sender, e) =>
+            {
+                Graphics graphics = e.Graphics;
+
+                // Schriftarten
+                Font titleFont = new Font("Segoe UI", 14, System.Drawing.FontStyle.Bold);
+                Font headerFont = new Font("Segoe UI", 12, System.Drawing.FontStyle.Bold);
+                Font contentFont = new Font("Segoe UI", 10);
+
+                float yOffset = 20; // Startposition
+
+                // Titel
+                graphics.DrawString("Tagesumsatz Auswertung", titleFont, Brushes.Black, new PointF(10, yOffset));
+                yOffset += titleFont.Height + 10;
+
+                // Datum
+                graphics.DrawString(DateTime.Now.ToString("dd.MM.yyyy"), headerFont, Brushes.Black, new PointF(10, yOffset));
+                yOffset += headerFont.Height + 5;
+
+                // Tabelle Header
+                graphics.DrawString("Name", headerFont, Brushes.Black, new PointF(10, yOffset));
+                graphics.DrawString("Tagesumsatz", headerFont, Brushes.Black, new PointF(150, yOffset));
+                yOffset += headerFont.Height + 5;
+
+                // Tagesumsatz Daten
+                foreach (var dailySalesInfo in DailySalesInfoList)
+                {
+                    graphics.DrawString(dailySalesInfo.Name, contentFont, Brushes.Black, new PointF(10, yOffset));
+                    graphics.DrawString($"{dailySalesInfo.DailySales:F2} €", contentFont, Brushes.Black, new PointF(150, yOffset));
+                    yOffset += contentFont.Height + 5;
+                }
+
+                // Gesamtumsatz
+                graphics.DrawString("Gesamtumsatz: " + TotalSalesTextBlock.Text, headerFont, Brushes.Black, new PointF(10, yOffset));
+            };
+
+            printDoc.Print(); // Startet den Druckvorgang
+        }
+
     }
 }
